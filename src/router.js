adecb6053476d04e19077240904aa6c58a0a9de3
GrapevineLin <546415457@qq.com>
2019-08-28 14:28:44 +0800

    基本结构

--- src/router.js
+++ src/router.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import GoodsList from './components/GoodsList'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      children: [{
+        path: 'goodslist',
+        name: 'goodslist',
+        component: GoodsList
+      }, {
+        path: 'cardslist',
+        name: 'cardslist',
+        component: () => import('./components/CardList.vue')
+      }]
     },
     {
       path: '/about',
@@ -17,7 +26,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: () => import( /* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-header class="header">WSLL后台管理系统</el-header>
+      <el-container>
+        <el-aside width="”200px”" class="side">
+          <el-menu router default-active="$route.path" class="el-menu-vertical-demo">
+            <el-menu-item index="/goodslist">
+              <i class="el-icon-goods"></i>
+              <span slot="title">商品管理</span>
+            </el-menu-item>
+            <el-menu-item index="/cardslist">
+              <i class="el-icon-goods"></i>
+              <span slot="title">购物车商品</span>
+            </el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main class="main">
+          <router-view />
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
+<style>
+.home {
+  height: 1000px;
+  /* background-color:red; */
+}
+.header {
+  background-color: #3080db;
+  text-align: center;
+}
+.side {
+  background-color: #a4caf6;
+  height: 400px;
+}
+.main {
+  /* background-color: #3080db; */
+}
+</style>
